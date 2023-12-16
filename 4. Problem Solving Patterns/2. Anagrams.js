d99946c3e5595785edc15907439c86c1a4ceb275
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-12-16 20:23:10 +0200

    Anagram solution added

---
+++ 4. Problem Solving Patterns/2. Anagrams.js
@@ -0,0 +1,57 @@
+// ANAGRAMS
+// Given two strings, write a function to determine if the second string is an anagram of the first.
+// An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+function validAnagram(str1, str2) {
+    if (!isInputValid()) {
+        return false;
+    }
+
+    const map1 = {};
+    const map2 = {};
+
+    const LEN = str1.length;
+
+    for (let i = 0; i < LEN; i++) {
+        const letter1 = str1[i];
+        const letter2 = str2[i];
+
+        map1[letter1] = (map1[letter1] || 0) + 1;
+        map2[letter2] = (map2[letter2] || 0) + 1;
+    }
+
+    for (const key in map1) {
+        if (map1[key] !== map2[key]) {
+            return false;
+        }
+    }
+
+    return true;
+
+    function isInputValid() {
+        if (typeof str1 !== 'string' ||
+        typeof str2 !== 'string' ||
+        str1.length !== str2.length
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+const result0 = validAnagram('', '');
+const result1 = validAnagram('aaz', 'zza');
+const result2 = validAnagram('anagram', 'nagaram');
+const result3 = validAnagram('rat', 'cat');
+const result4 = validAnagram('awesome', 'awesom');
+const result5 = validAnagram('qwerty', 'qeywrt');
+const result6 = validAnagram('texttwisttime', 'timetwisttext');
+
+console.log(result0); // true
+console.log(result1); // false
+console.log(result2); // true
+console.log(result3); // false
+console.log(result4); // false
+console.log(result5); // true
+console.log(result6); // true
