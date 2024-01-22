1d3ba39a1830e6359f6df48b06bda60832192d2e
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2024-01-22 23:51:35 +0200

    linked list

--- 19. Singly Linked Lists/2.js
+++ 19. Singly Linked Lists/2.js
@@ -1,1 +1,47 @@
-class
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Add a new node to the end of the list
+  append(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
+  // Print the elements of the list
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+// Example usage:
+const linkedList = new LinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+linkedList.print();
